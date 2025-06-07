4aaa4c6172fe3d7b2714396e0f1f41c07e7007ba
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-08 00:29:31 +0530

    Insert data Into

--- Project1_Inventory/insert_data.sql
+++ Project1_Inventory/insert_data.sql
@@ -1,17 +1,16 @@
--- Insert Data into Customers
-INSERT INTO customers VALUES
-(1, 'Ali', 'UAE'),
-(2, 'Zara', 'Qatar'),
-(3, 'John', 'KSA');
+INSERT INTO Categories (CategoryID, CategoryName) VALUES
+(1, 'Electronics'),
+(2, 'Groceries'),
+(3, 'Stationery');
 
--- Insert Data into Products
-INSERT INTO products VALUES
-(101, 'Laptop', 3000.00, 10),
-(102, 'Mouse', 50.00, 100),
-(103, 'Keyboard', 120.00, 60);
-
--- Insert Data into Orders
-INSERT INTO orders VALUES
-(1001, 1, 101, '2024-05-01', 1, 3000.00),
-(1002, 2, 102, '2024-05-02', 2, 100.00),
-(1003, 3, 103, '2024-05-03', 1, 120.00);
+INSERT INTO Products (ProductID, ProductName, Price, Quantity, CategoryID) VALUES
+(101, 'Laptop', 1200.00, 5, 1),
+(102, 'Smartphone', 850.00, 8, 1),
+(103, 'LED Bulb', 2.50, 100, 2),
+(104, 'Rice (5kg)', 15.00, 20, 2),
+(105, 'Milk Pack', 1.20, 50, 2),
+(106, 'Notebook', 1.00, 75, 3),
+(107, 'Ball Pen', 0.50, 200, 3),
+(108, 'Charger', 20.00, 12, 1),
+(109, 'Sugar (2kg)', 3.50, 25, 2),
+(110, 'Stapler', 2.00, 15, 3);
