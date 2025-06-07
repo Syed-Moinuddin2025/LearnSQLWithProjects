dd93e652d5fe12d0ea5133be094cdd745d847a41
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-08 00:28:53 +0530

    Create Table

--- Project1_Inventory/create_tables.sql
+++ Project1_Inventory/create_tables.sql
@@ -1,35 +1,13 @@
--- Project 1: Mini Inventory Database
-
-DROP DATABASE IF EXISTS InventoryDB;
-
--- Step 1: Create Database
-CREATE DATABASE InventoryDB;
-GO
-
--- Step 2: Use the database
-USE InventoryDB;
-GO
-
--- Step 3: Create Customers Table
-CREATE TABLE customers (
-    customer_id INT PRIMARY KEY,
-    first_name NVARCHAR(50),
-    country NVARCHAR(50)
+ CREATE TABLE Categories (
+    CategoryID INT PRIMARY KEY,
+    CategoryName NVARCHAR(100) NOT NULL
 );
 
--- Step 4: Create Products Table
-CREATE TABLE products (
-    product_id INT PRIMARY KEY,
-    product_name NVARCHAR(100),
-    price DECIMAL(10,2),
-    stock_quantity INT
-);
--- Step 5: Create Orders Table (with foreign keys)
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT FOREIGN KEY REFERENCES customers(customer_id),
-    product_id INT FOREIGN KEY REFERENCES products(product_id),
-    order_date DATE,
-    quantity INT,
-    sales DECIMAL(10,2)
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName NVARCHAR(100) NOT NULL,
+    Price DECIMAL(10,2),
+    Quantity INT,
+    CategoryID INT FOREIGN KEY REFERENCES Categories(CategoryID)
 );
