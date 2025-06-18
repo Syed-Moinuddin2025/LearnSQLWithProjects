d817988106f7248b710507104ded21a74f91d9b2
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-18 16:55:51 +0530

    SQL Data Insert

---
+++ SQL_Project1_Inventory/02_insert_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Categories (CategoryID, CategoryName) VALUES
+(1, 'Electronics'),
+(2, 'Groceries'),
+(3, 'Stationery');
+
+INSERT INTO Products (ProductID, ProductName, Price, Quantity, CategoryID) VALUES
+(101, 'Laptop', 1200.00, 5, 1),
+(102, 'Smartphone', 850.00, 8, 1),
+(103, 'LED Bulb', 2.50, 100, 2),
+(104, 'Rice (5kg)', 15.00, 20, 2),
+(105, 'Milk Pack', 1.20, 50, 2),
+(106, 'Notebook', 1.00, 75, 3),
+(107, 'Ball Pen', 0.50, 200, 3),
+(108, 'Charger', 20.00, 12, 1),
+(109, 'Sugar (2kg)', 3.50, 25, 2),
+(110, 'Stapler', 2.00, 15, 3);
