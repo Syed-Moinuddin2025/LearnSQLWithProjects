7f8a4765e943dbdfa5c05c530e8a3d9f6acf0a93
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-20 10:46:40 +0530

    Solution 3

--- python_basics/02_conditionals/03_solution.py
+++ python_basics/02_conditionals/03_solution.py
@@ -1,7 +1,7 @@
 # v3. Grade Calculator
 # Problem: Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
 
-score = 90
+score = 95
 
 if score >  100:
 
