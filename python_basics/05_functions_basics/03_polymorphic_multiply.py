46a529112d524f83d45eb0b09c80c3abfb77dfec
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-21 18:52:14 +0530

    3. Polymorphism in Functions

---
+++ python_basics/05_functions_basics/03_polymorphic_multiply.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    """Multiplies numbers or repeats a string."""
+    return a * b
+
+# Example usages:
+print(multiply(5, 3))         # 15 → number * number
+print(multiply("Hi ", 3))     # "Hi Hi Hi " → string * number
+print(multiply(3, "Hello "))  # "Hello Hello Hello " → number * string
